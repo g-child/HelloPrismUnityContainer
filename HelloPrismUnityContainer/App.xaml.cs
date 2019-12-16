364b22b926237bbb0f9cb9f977c11eecf33cc401
Hiroki Kaneko <hkaneko@it-brn.com>
2019-12-16 13:49:25 +0900

    Add project files.

---
+++ HelloPrismUnityContainer/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using HelloPrismUnityContainer.Views;
+using Prism.Mvvm;
+using System.Windows;
+using Unity;
+
+namespace HelloPrismUnityContainer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IUnityContainer Container { get; } = new UnityContainer();
+
+        private void Application_Startup(object sender, StartupEventArgs eventArgs)
+        {
+            ViewModelLocationProvider.SetDefaultViewModelFactory(x => Container.Resolve(x));
+            Container.Resolve<MainWindow>().Show();
+        }
+    }
+}
---
+++ HelloPrismUnityContainer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
+
+namespace HelloPrismUnityContainer.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public DelegateCommand AlertCommand { get; }
+        public InteractionRequest<INotification> AlertRequest { get; } = new InteractionRequest<INotification>();
+
+        private string _input;
+        public string Input
+        {
+            get => _input;
+            set
+            {
+                SetProperty(ref _input, value);
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            AlertCommand = new DelegateCommand(() => AlertRequest.Raise(new Notification { Title = "Alert !", Content = Input }));
+        }
+    }
+}
